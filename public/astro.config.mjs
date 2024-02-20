4b2b2252470bd126ca3d408b65c494c4b9b203a3
Gokul V <gokulv@192.168.1.12>
2024-02-20 23:03:59 +0530

    My Images

--- public/astro.config.mjs
+++ public/astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://gokulvenkats.github.io',
-  // base: '/gokulvenkat.com',
+  site: "https://gokulvenkats.github.io",
+  base: "",
 });
